5927861c4a2cfa0a2fbb415ff9241786f81a8a14
Peter Zumbruch <p.zumbruch@gsi.de>
2012-11-13 11:02:29 +0000

    reordering of function pointers pre processor macro for variable arguments
    git-svn-id: https://subversion.gsi.de/epics/trunk/hadcon/API-Slave@337 50339f28-a4cd-11e1-ab1f-8360dde1fe65

--- api.h
+++ api.h
@@ -29,8 +29,11 @@ extern struct uartStruct uartFrame;
 extern struct uartStruct *ptr_uartStruct;
 
 
-/* Implemented functions */
+/* Implemented functions
+ * and
+ * corresponding function pointers*/
 void Process_Uart_Event( void );
+extern void (*Process_Uart_Event_p)( void );
 
 int8_t Check_Error( struct uartStruct *ptr_uartStruct ); /*this function checks error for the received parameter */
 
@@ -67,8 +70,10 @@ void Unsubscribe_Message( struct uartStruct *PtrFrame ); /* function for the com
 int8_t UART0_Init( void ); /* initializieren serial communication */
 
 void UART0_Transmit( unsigned char c );/* function send  data direction to cpu */
+extern void (*UART0_Transmit_p)( uint8_t );
 
 int16_t UART0_Send_Message_String( char *tmp_str, uint16_t maxSize );/* help function for the output of register contents in CPU */
+extern int16_t (*UART0_Send_Message_String_p)( char *, uint16_t );
 
 int8_t UART0_Send_Message_String_woLF( char *tmp_str, uint32_t maxSize );/* help function for the output of register contents in CPU */
 
@@ -81,12 +86,15 @@ void Reset_UartStruct( struct uartStruct *ptr_uartStruct ); /* resets all values
 void clearUartStruct( struct uartStruct *ptr_uartStruct ); /* resets all values of uartStruct*/
 
 uint8_t CommunicationError( uint8_t errorType, const int16_t errorIndex, const uint8_t flag_printCommand, const prog_char *alternativeErrorMessage, ... );
+extern uint8_t (*CommunicationError_p)(uint8_t, const int16_t, const uint8_t, const prog_char*, ...);
 
 void printDebug( uint8_t debugLevel, uint32_t debugMaskIndex, uint32_t line, const prog_char* file, const prog_char *format, ...);
+extern void (*printDebug_p)(uint8_t, uint32_t, uint32_t, const prog_char*, const prog_char*, ...);
 
 uint8_t createReceiveHeader( struct uartStruct *ptr_uartStruct, char message_string[], uint16_t size );
 
 uint16_t clearString( char mystring[], uint16_t length );
+extern uint16_t (*clearString_p)( char[], uint16_t );
 
 void toggle_pin( unsigned char pin_number );
 
@@ -99,6 +107,30 @@ int8_t getNumericValueFromParameter(uint8_t parameterIndex, uint32_t *ptr_value)
 void reset(struct uartStruct *ptr_uartStruct);
 
 uint8_t initUartStruct(struct uartStruct *ptr_myUartStruct);
+
+/* getting number of variable arguments
+ *
+ * found at https://groups.google.com/forum/?fromgroups=#!topic/comp.std.c/d-6Mj5Lko_s
+ */
+
+#define PP_NARG(...) PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
+#define PP_NARG_(...) PP_ARG_N(__VA_ARGS__)
+#define PP_ARG_N( \
+          _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
+         _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+         _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+         _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+         _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+         _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+         _61,_62,_63,N,...) N
+#define PP_RSEQ_N() \
+         63,62,61,60,                   \
+         59,58,57,56,55,54,53,52,51,50, \
+         49,48,47,46,45,44,43,42,41,40, \
+         39,38,37,36,35,34,33,32,31,30, \
+         29,28,27,26,25,24,23,22,21,20, \
+         19,18,17,16,15,14,13,12,11,10, \
+         9,8,7,6,5,4,3,2,1,0
 
 #ifndef API_CONSTANTS_H_
 
@@ -297,13 +329,6 @@ enum ERRs
 extern const char *debugLevelNames[] PROGMEM;
 extern const char *debugSystemNames[] PROGMEM;
 
-/*function pointers*/
-extern void (*printDebug_p)(uint8_t, uint32_t, uint32_t, const prog_char*, const prog_char*, ...);
-extern uint8_t (*CommunicationError_p)(uint8_t, const int16_t, const uint8_t, const prog_char*, ...);
-extern int16_t (*UART0_Send_Message_String_p)( char *, uint16_t );
-extern void (*UART0_Transmit_p)( uint8_t );
-extern void (*Process_Uart_Event_p)( void );
-extern uint16_t (*clearString_p)( char[], uint16_t );
 
 #endif
 #endif
