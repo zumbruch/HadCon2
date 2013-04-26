00c354e644f6e21ec827b4c06744c3b502f39d46
Peter Zumbruch <p.zumbruch@gsi.de>
2013-04-26 11:53:45 +0000

    - introduced enums for the CAN communication state - moved can relevant section from api into can code - refactoring of names
    git-svn-id: https://subversion.gsi.de/epics/trunk/hadcon/API-Slave@377 50339f28-a4cd-11e1-ab1f-8360dde1fe65

--- can.h
+++ can.h
@@ -38,13 +38,44 @@ extern struct canStruct canFrame;
 extern struct canStruct *ptr_canStruct;
 extern int32_t canDefaultBaudRate;
 
+enum canState {
+	canState_IDLE = 0,
+	canState_RXOK = 1,
+	canState_MOB_ERROR = 2,
+	canState_GENERAL_ERROR = 3,
+	canState_MAXIMUM_INDEX
+	};
+
+extern const char *can_error[] PROGMEM;
+enum ce_index
+{
+   CAN_ERROR_Can_Bus_is_off = 0,
+   CAN_ERROR_Can_Bus_is_passive,
+   CAN_ERROR_Can_Bus_is_on,
+   CAN_ERROR_MOb_Bit_Error,
+   CAN_ERROR_MOb_Stuff_Error,
+   CAN_ERROR_MOb_CRC_Error,
+   CAN_ERROR_MOb_Form_Error,
+   CAN_ERROR_MOb_Acknowledgement_Error,
+   CAN_ERROR_CAN_was_not_successfully_initialized,
+   CAN_ERROR_CAN_communication_timeout,
+   CAN_ERROR_MAXIMUM_INDEX
+};
+
+extern const char *mob_error[] PROGMEM;
+enum me_index
+{
+   MOB_ERROR_all_mailboxes_already_in_use = 0,
+   MOB_ERROR_message_ID_not_found,
+   MOB_ERROR_this_message_already_exists,
+   MOB_ERROR_MAXIMUM_INDEX
+};
+
 /* implemented functions */
 
 int8_t canInit( int32_t Baudrate ); /* initialize CAN communication*/
 
-void enableCan( void );
-
-void Convert_canFormat_to_UartFormat( struct canStruct *ptr_canStruct );/*This function grabs the CAN data received in a string*/
+void canEnableCan( void );
 
 void canGetGeneralStatusError( void ); /* state on the CAN channel */
 
@@ -54,13 +85,13 @@ void canGetMObError( void ); /* can error for the communication */
 
 uint8_t canIsMObErrorAndAcknowledge( void );
 
-uint8_t canErrorHandling( void );
+uint8_t canErrorHandling( uint8_t error );
 
 void canSetMObCanIDandMask(uint32_t id, uint32_t mask, uint8_t enableRTRMaskBitComparison_flag, uint8_t enableIDExtensionMaskBitComparison_flag);
 
-int8_t Get_FreeMob( void ); /* searches free Mob */
+int8_t canGetFreeMob( void ); /* searches free Mob */
 
-void clearCanRegisters( void ); /* clean up of parameters */
+void canClearCanRegisters( void ); /* clean up of parameters */
 
 void canResetParametersCANSend( void );/*this function*/
 
@@ -74,11 +105,19 @@ void canClearCanStruct( struct canStruct *ptr_canStruct); /*resets the canStruct
 
 void canConvertCanFrameToUartFormat( struct canStruct *ptr_canStruct );/* this function collects the various CAN data in a string */
 
-int setCanBaudRate( const uint32_t rate, const uint32_t freq );
+void canSendRemoteTransmissionRequestMessage( struct uartStruct *PtrFrame ); /* function for the SEND command name and RTR set, initialization of the registers with elements of the structure uartStruct*/
+
+void canSendMessage( struct uartStruct *PtrFrame );/*function for the SEND command name and RTR is not set, initialization of the registers with elements of the structure uartStruct*/
+
+void canSubscribeMessage( struct uartStruct *PtrFrame ); /* function for the command name SUBS, initialization of the registers with elements of the structure uartStruct */
+
+void canUnsubscribeMessage( struct uartStruct *PtrFrame ); /* function for the command name USUB, initialization of the registers with elements of the structure uartStruct */
+
+int canSetCanBaudRate( const uint32_t rate, const uint32_t freq );
 
 uint8_t canCheckInputParameterError( uartMessage *ptr);
 
-uint8_t setCanBitTimingTQUnits(uint8_t numberOfTimeQuanta, uint16_t freq2BaudRatio, int8_t bitRatePreScaler,
+uint8_t canSetCanBitTimingTQUnits(uint8_t numberOfTimeQuanta, uint16_t freq2BaudRatio, int8_t bitRatePreScaler,
 		                       uint8_t propagationTimeSegment, uint8_t phaseSegment1, uint8_t phaseSegment2, uint8_t syncJumpWidth,
 				               uint8_t multipleSamplePointSampling_flag, uint8_t autoCorrectBaudRatePreScalerNull_flag);
 uint8_t canBitTimingTQBasicBoundaryChecks(uint8_t propagationTimeSegment, uint8_t phaseSegment1, uint8_t phaseSegment2, uint8_t syncJumpWidth);
