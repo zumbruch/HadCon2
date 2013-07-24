47b28833d388232eb3d9507b95ba9ec0977067ed
Peter Zumbruch <p.zumbruch@gsi.de>
2013-07-24 14:58:28 +0200

    help_twis: made use of helpShowCommandOrResponse command

--- help_twis.c
+++ help_twis.c
@@ -19,25 +19,23 @@
 #include "api_debug.h"
 #include "help_twis.h"
 
-//static const char filename[] 		PROGMEM = __FILE__;
+static const char arguments[] 		PROGMEM = "<address> <data length> <data bytes1 ... 8>";
 
 void help_twis(char *currentReceiveHeader, char *currentCommandKeyword)
 {
 	if (NULL == currentReceiveHeader)  { return; }
 	if (NULL == currentCommandKeyword) { return; }
 
-	snprintf_P(uart_message_string, BUFFER_SIZE - 1,
-			PSTR("%s TWI / I2C access (dummy name) "), message );
+	strncat(uart_message_string, message, BUFFER_SIZE - 1 );
+	strncat_P(uart_message_string, PSTR(" TWI / I2C access (dummy name)"), BUFFER_SIZE - 1 );
 	UART0_Send_Message_String_p(NULL,0);
-	snprintf_P(uart_message_string, BUFFER_SIZE - 1,
-			PSTR("%s command : %s <1/0> <address> <data length> <data bytes1 ... 8>"), message, currentCommandKeyword );
-	UART0_Send_Message_String_p(NULL,0);
-	snprintf_P(uart_message_string, BUFFER_SIZE - 1,
-			PSTR("%s response (write \"0\"): %s 0 <address> <data length> <data bytes1 ... 8> -OK-"), message, currentReceiveHeader );
-	UART0_Send_Message_String_p(NULL,0);
-	snprintf_P(uart_message_string, BUFFER_SIZE - 1,
-			PSTR("%s response  (read \"1\"): %s 1 <address> <data length> <data bytes1 ... 8> "), message, currentReceiveHeader );
-	UART0_Send_Message_String_p(NULL,0);
+
+	/* command */
+	helpShowCommandOrResponse_p (                NULL, PSTR("           "),   PSTR("<1/0> %S"),arguments);
+
+	/* response */
+	helpShowCommandOrResponse_p (currentReceiveHeader, PSTR("(write \"0\")"), PSTR("0 %S -OK-"),arguments);
+	helpShowCommandOrResponse_p (currentReceiveHeader, PSTR(" (read \"1\")"), PSTR("1 %S"), arguments);
 }
 
 
