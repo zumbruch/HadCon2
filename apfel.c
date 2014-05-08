12b72829b641f818ba9f68476e2e1534426335dd
Peter Zumbruch <p.zumbruch@gsi.de>
2014-05-09 01:31:16 +0200

    implemented higher level apfel commands

--- apfel.c
+++ apfel.c
@@ -145,7 +145,7 @@ apiCommandResult apfelWriteBit(uint8_t bit, uint8_t portAddress, uint8_t ss, uin
 	return apiCommandResult_SUCCESS_QUIET;
 }
 
-apiCommandResult readBitSequence(uint8_t nBits, uint32_t* bits, uint8_t portAddress, uint8_t ss, uint8_t pinDIN, uint8_t pinCLK, uint8_t pinSS)
+apiCommandResult apfelReadBitSequence(uint8_t nBits, uint32_t* bits, uint8_t portAddress, uint8_t ss, uint8_t pinDIN, uint8_t pinCLK, uint8_t pinSS)
 {
 	if (pinCLK > 7 || pinDIN > 7 || pinSS > 7)
 	{
@@ -220,7 +220,7 @@ apiCommandResult readBitSequence(uint8_t nBits, uint32_t* bits, uint8_t portAddr
 	return apiCommandResult_SUCCESS_QUIET;
 }
 
-apiCommandResult apfelWriteClockSequence(uint8_t num, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
+apiCommandResult apfelWriteClockSequence(uint32_t num, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
 {
 	while (num)
 	{
@@ -245,7 +245,7 @@ uint8_t apfelSetClockAndDataLine( uint8_t portAddress, uint8_t value, uint8_t ma
 }
 
 
-apiCommandResult apfelClearDataInput(uint8_t num, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
+apiCommandResult apfelClearDataInput(uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
 {
 	if (pinCLK > 7 || pinDOUT > 7 || pinSS > 7)
 	{
@@ -301,6 +301,169 @@ apiCommandResult apfelWriteBitSequence(uint8_t num, uint32_t data, uint8_t portA
 	return apiCommandResult_SUCCESS_QUIET;
 }
 
+//mandatory header to all command sequences
+apiCommandResult apfelStartStreamHeader(uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
+{
+	if (pinCLK > 7 || pinDOUT > 7 || pinSS > 7)
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+	uint8_t mask = (1 << pinCLK | 1 << pinDOUT | 1 << pinSS);
+
+	ss  = ss?1:0;
+
+	if ( apiCommandResult_FAILURE > apfelClearDataInput(portAddress, ss, pinCLK, pinDOUT, pinSS)){ return apiCommandResult_FAILURE_QUIET; }
+
+	//clock Low + data low
+	if ( false == apfelSetClockAndDataLine( portAddress, ((uint8_t)(0 << pinCLK | 0 << pinDOUT | ss << pinSS)), mask)) { return apiCommandResult_FAILURE_QUIET; }
+	//# clock Low  + data high
+	if ( false == apfelSetClockAndDataLine( portAddress, ((uint8_t)(0 << pinCLK | 1 << pinDOUT | ss << pinSS)), mask)) { return apiCommandResult_FAILURE_QUIET; }
+	//# clock High + data high
+	if ( false == apfelSetClockAndDataLine( portAddress, ((uint8_t)(1 << pinCLK | 1 << pinDOUT | ss << pinSS)), mask)) { return apiCommandResult_FAILURE_QUIET; }
+	//# clock low + data high
+	if ( false == apfelSetClockAndDataLine( portAddress, ((uint8_t)(0 << pinCLK | 1 << pinDOUT | ss << pinSS)), mask)) { return apiCommandResult_FAILURE_QUIET; }
+	//# clock low  + data low
+	if ( false == apfelSetClockAndDataLine( portAddress, ((uint8_t)(0 << pinCLK | 0 << pinDOUT | ss << pinSS)), mask)) { return apiCommandResult_FAILURE_QUIET; }
+
+	return apiCommandResult_SUCCESS_QUIET;
+}
+
 /*
  * High Level commands
  */
+apiCommandResult apfelCommandSet(uint16_t command, uint16_t value, uint8_t chipID, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
+{
+	if (pinCLK > 7 || pinDOUT > 7 || pinSS > 7)
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+	apfelStartStreamHeader(portAddress, ss, pinCLK, pinDOUT, pinSS);
+	//# command + ...
+	apfelWriteBitSequence( APFEL_N_COMMAND_BITS, command, portAddress, ss, pinCLK, pinDOUT, pinSS);
+	// value
+	apfelWriteBitSequence( APFEL_N_VALUE_BITS, value, portAddress, ss, pinCLK, pinDOUT, pinSS);
+	//# chipId
+	apfelWriteBitSequence( APFEL_N_CHIP_ID_BITS, chipID, portAddress, ss, pinCLK, pinDOUT, pinSS);
+
+	return apiCommandResult_SUCCESS_QUIET;
+}
+
+
+//#setDac dacNr[1..4] value[ 0 ... 3FF ] chipID[0 ... FF]
+apiCommandResult apfelSetDac(uint16_t value, uint8_t dacNr, uint8_t chipID, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
+{
+	if ( 1 > dacNr || dacNr > 4 )
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+	if (value > (0x1 << APFEL_N_VALUE_BITS) -1 )
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+    apfelCommandSet(APFEL_COMMAND_SET_DAC + dacNr, value, chipID, portAddress, ss, pinCLK, pinDOUT, pinSS);
+
+	//#3 intermediate clock cycles equiv. 3 writeDataLow
+	apfelWriteClockSequence(3, portAddress, ss, pinCLK, pinDOUT, pinSS);
+
+	return apiCommandResult_SUCCESS_QUIET;
+}
+
+//#readDac dacNr[1..4] chipID[0 ... FF]
+apiCommandResult apfelReadDac(uint32_t* dacValue, uint8_t dacNr, uint8_t chipID,  uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS, uint8_t pinDIN)
+{
+	if ( NULL == dacValue )
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+	if (pinDIN > 7)
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+	if ( 1 > dacNr || dacNr > 4 )
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+    apfelCommandSet(APFEL_COMMAND_READ_DAC + dacNr, 0, chipID, portAddress, ss, pinCLK, pinDOUT, pinSS);
+
+	apfelReadBitSequence( 2 + APFEL_N_VALUE_BITS + 3, dacValue, portAddress, ss, pinDIN, pinCLK, pinSS);
+    //cut out real dacValue
+	*dacValue = (*dacValue & ((0x1 << APFEL_N_VALUE_BITS) -1)) >> 3;
+
+	return apiCommandResult_SUCCESS_QUIET;
+}
+
+//#autoCalibration chipId[0 ... FF]
+apiCommandResult apfelAutoCalibration(uint8_t chipID, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
+{
+
+    apfelCommandSet(APFEL_COMMAND_AUTOCALIBRATION, 0, chipID, portAddress, ss, pinCLK, pinDOUT, pinSS);
+
+    //#calibration sequence clocks 4 * 10bit DAC
+    apfelWriteClockSequence( (1 << APFEL_N_VALUE_BITS) << 2, portAddress, ss, pinCLK, pinDOUT, pinSS);
+
+	return apiCommandResult_SUCCESS_QUIET;
+}
+
+//#testPulseSequence pulseHeightPattern[0 ... 1F](<<1/5) chipId[0 ... FF]
+apiCommandResult apfelTestPulseSequence(uint16_t pulseHeightPattern, uint8_t chipID, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
+{
+	if (	(((pulseHeightPattern >> 1) & ((0x1 << 5)-1)) > APFEL_TEST_PULSE_HEIGHT_PATTERN_MAX) ||
+		    (((pulseHeightPattern >> 5) & ((0x1 << 5)-1)) > APFEL_TEST_PULSE_HEIGHT_PATTERN_MAX))
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+    apfelCommandSet(APFEL_COMMAND_TESTPULSE, pulseHeightPattern, chipID, portAddress, ss, pinCLK, pinDOUT, pinSS);
+
+    apfelWriteClockSequence( 1, portAddress, ss, pinCLK, pinDOUT, pinSS);
+
+	return apiCommandResult_SUCCESS_QUIET;
+}
+
+//#testPulse pulseHeight[0 ... 1F] channel[1 .. 2 ] chipId[0 ... FF]
+apiCommandResult apfelTestPulse(uint8_t pulseHeight, uint8_t channel, uint8_t chipID, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
+{
+	if (pulseHeight > APFEL_TEST_PULSE_HEIGHT_PATTERN_MAX)
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+	if (1 > channel || channel > 2)
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+	apfelTestPulseSequence( pulseHeight << (1 + (channel == 1)?0:5), chipID, portAddress, ss, pinCLK, pinDOUT, pinSS);
+	apfelTestPulseSequence( 0                                      , chipID, portAddress, ss, pinCLK, pinDOUT, pinSS);
+
+	return apiCommandResult_SUCCESS_QUIET;
+}
+
+//#setAmplitude channelId[1 ... 2] chipId[0 ... FF]
+apiCommandResult apfelSetAmplitude(uint8_t channelId, uint8_t chipID, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
+{
+	if (1 > channelId || channelId > 2)
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+    return apfelCommandSet(APFEL_COMMAND_SET_AMPLITUDE + 2 - channelId, 0, chipID, portAddress, ss, pinCLK, pinDOUT, pinSS);
+}
+
+//#resetAmplitude channelId[1 ... 2] chipId[0 ... FF]
+apiCommandResult apfelResetAmplitude(uint8_t channelId, uint8_t chipID, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
+{
+	if (1 > channelId || channelId > 2)
+	{
+		return apiCommandResult_FAILURE_QUIET;
+	}
+
+    return apfelCommandSet(APFEL_COMMAND_RESET_AMPLITUDE + ((channelId -1)*2), 0, chipID, portAddress, ss, pinCLK, pinDOUT, pinSS);
+}
