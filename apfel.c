f9fec6772057d94f812cdb0e64eaa73002616186
Peter Zumbruch <p.zumbruch@gsi.de>
2014-05-09 17:35:34 +0200

    apfel.c: removed old write/read code

--- apfel.c
+++ apfel.c
@@ -19,17 +19,17 @@
 #include <avr/iocan128.h>
 #endif
 
-// initial configuration for chipselectarray -> all chipselects are unused
-apfelPin apfelChipSelectArray[APFEL_CHIPSELECT_MAXIMUM] = { { 0 , 0 , false  },
-					    { 0 , 0 , false  },
-					    { 0 , 0 , false  },
-					    { 0 , 0 , false  },
-					    { 0 , 0 , false  },
-					    { 0 , 0 , false  },
-					    { 0 , 0 , false  },
-					    { 0 , 0 , false  } };
-
-uint8_t apfelInternalChipSelectMask = 0;
+//// initial configuration for chipselectarray -> all chipselects are unused
+//apfelPin apfelChipSelectArray[APFEL_CHIPSELECT_MAXIMUM] = { { 0 , 0 , false  },
+//					    { 0 , 0 , false  },
+//					    { 0 , 0 , false  },
+//					    { 0 , 0 , false  },
+//					    { 0 , 0 , false  },
+//					    { 0 , 0 , false  },
+//					    { 0 , 0 , false  },
+//					    { 0 , 0 , false  } };
+//
+//uint8_t apfelInternalChipSelectMask = 0;
 
 
 
@@ -40,55 +40,6 @@ void apfelInit(void)
 	uint8_t i;
 
 	apfelUsToDelay = APFEL_DEFAULT_US_TO_DELAY;
-
-}
-
-apiCommandResult apfelWritePortA(uint8_t value, uint8_t mask)
-{
-	return apfelWritePort(value, PORTA, mask);
-}
-
-apiCommandResult apfelWritePort(uint8_t value, uint8_t portAddress, uint8_t mask)
-{
-	uint8_t readback_register = 0xFF; //dummy value;
-	//uint8_t read = REGISTER_READ_FROM_8BIT_REGISTER(portAddress);
-	uint8_t read = registerReadFrom8bitRegister(portAddress);
-
-	// just modify the masked out bits and keep the rest as is:
-	// read & (~mask) | ( value & mask ) => read ^ (mask & ( read ^ value))
-	uint8_t set = read ^ ( mask & ( read ^ value));
-	//readback_register = REGISTER_WRITE_INTO_8BIT_REGISTER_AND_READBACK(portAddress, set);
-	readback_register = registerWriteInto8bitRegister(portAddress, set);
-    _delay_us(apfelUsToDelay);
-
-	if (readback_register == set)
-	{
-		return apiCommandResult_SUCCESS_QUIET;
-	}
-	else
-	{
-		return apiCommandResult_FAILURE;
-	}
-}
-
-apiCommandResult apfelReadPortA(uint8_t *value)
-{
-	return apfelReadPort(value, PORTA);
-}
-
-apiCommandResult apfelReadPort(uint8_t *value, uint8_t portAddress)
-{
-	uint8_t apiCommandResult = apiCommandResult_FAILURE;
-
-	if (NULL == value)
-	{
-		CommunicationError_p(ERRA, GENERAL_ERROR_value_has_invalid_type, TRUE, NULL);
-		return apiCommandResult_FAILURE_QUIET;
-	}
-
-	*value = registerReadFrom8bitRegister(portAddress);
-    _delay_us(apfelUsToDelay);
-	return apiCommandResult_SUCCESS_QUIET;
 }
 
 apiCommandResult apfelWriteBit(uint8_t bit, uint8_t portAddress, uint8_t ss, uint8_t pinCLK, uint8_t pinDOUT, uint8_t pinSS)
@@ -113,34 +64,6 @@ apiCommandResult apfelWriteBit(uint8_t bit, uint8_t portAddress, uint8_t ss, uin
 	if ( false == apfelSetClockAndDataLine( portAddress, ((uint8_t)(1 << pinCLK | 0   << pinDOUT | ss << pinSS)), mask)) { return apiCommandResult_FAILURE_QUIET; }
 	//# clock low  + data low
 	if ( false == apfelSetClockAndDataLine( portAddress, ((uint8_t)(0 << pinCLK | 0   << pinDOUT | ss << pinSS)), mask)) { return apiCommandResult_FAILURE_QUIET; }
-
-#if 0
-	//clock Low + data low
-	if (result < apiCommandResult_FAILURE)
-	{
-		result = apfelWritePort((0 << pinCLK | 0 << pinDOUT | ss << pinSS), portAddress, mask);
-	}
-	//# clock Low  + data high
-	if (result < apiCommandResult_FAILURE)
-	{
-		result = apfelWritePort((0 << pinCLK | bit << pinDOUT | ss << pinSS), portAddress, mask);
-	}
-	//# clock High + data high
-	if (result < apiCommandResult_FAILURE)
-	{
-		result = apfelWritePort((1 << pinCLK | bit << pinDOUT | ss << pinSS), portAddress, mask);
-	}
-	//# clock High + data low
-	if (result < apiCommandResult_FAILURE)
-	{
-		result = apfelWritePort((1 << pinCLK | 0 << pinDOUT | ss << pinSS), portAddress, mask);
-	}
-	//# clock low  + data low
-	if (result < apiCommandResult_FAILURE)
-	{
-		result = apfelWritePort((0 << pinCLK | 0 << pinDOUT | ss << pinSS), portAddress, mask);
-	}
-#endif
 
 	return apiCommandResult_SUCCESS_QUIET;
 }
