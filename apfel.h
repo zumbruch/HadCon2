0386beed3002ab2453e51326e86bfcec4c1a5e40
zumbruch <p.zumbruch@gsi.de>
2015-01-22 11:22:49 +0100

    apfel.h introduced typedef struct apfelAddress

--- apfel.h
+++ apfel.h
@@ -6,61 +6,6 @@
  */
 
 
-/*
- * Normal workflow:
- *-----------------
- * apfelInit(); // initialize APFEL and sets PORTB0 as APFEL_CHIPSELECT0 and activates it in the internal chipselect mask
- * apfelEnable(); // enables apfel
- * apfelPurgeWriteData(); // clean both data buffers for safety
- * apfelPurgeReadData();
- * apfelAddWriteData( value ) // adds one value to the write buffer
- * apfelWriteAndReadWithChipSelect(APFEL_MSBYTE_FIRST, ( 1 << APFEL_CHIPSELECT0 ) ); // sets APFEL_CHIPSELECT0 to active and transmits the write buffer
- * myData = apfelGetReadData(); // the read buffer is automatically filled by the WriteAndRead functions and can be accessed by apfelGetReadData();
- *-------------------------------------------------
- *
- * Setting new configuration:
- *---------------------------
- * apfelConfigUnion newConfig;
- * newConfig = apfelGetConfiguration(); // get old configuration
- * newConfig.bits.bSpi2x = 1; // sets bSpi2x bit for double speed operation
- * newConfig.data = 0xXXXX // for accessing the whole 16 bits at once. be careful because you can easily influence the master- and enable bit!
- * apfelSetConfiguration(newConfig); // set the new configration
- *-------------------------------------------------
- *
- * Add a new chipselect line to the chipSelectArray:
- *---------------------------
- * apfelAddChipSelect(&PORTC, PC3, APFEL_CHIPSELECT1); // adding PORTC3 as APFEL_CHIPSELECT1. PORTB0 is APFEL_CHIPSELECT0 by default but can be removed by apfelRemoveChipSelect(APFEL_CHIPSELECT0) and a new PORT and PIN can be assigned for APFEL_CHIPSELECT0
- *
- *
- * Which chipselects are used:
- *----------------------------
- * uint8_t currentStatus;
- * currentStatus = apfelChipSelectArrayStatus(); // returns one byte, if(bit0) -> chipselect0 is in use, if(bit1) -> chipselect1 is in use, etc...
- *-------------------------------------------------
- *
- * Which chipselects are masked by the internal mask:
- *---------------------------------------------------
- * uint8_t currentInternalMask;
- * currentInternalMask = apfelGetInternalChipSelectMask(); // returns one byte, if(bit0) -> chipselect0 is masked, if(bit1) -> chipselect1 is masked, etc...
- *-------------------------------------------------
- *
- * Manual influence of the chipselect lines:
- *--------------------------------------------
- * apfelReleaseAllChipSelectLines(); // releases all usable chipselect lines
- * // suppose CS0 to CS3 are internally masked and usable present in the chipSelectArray
- * apfelSetChosenChipSelect(APFEL_MASK_ALL_CHIPSELECTS); // all internally masked chipselects will be set to active
- * apfelReleaseChosenChipSelect( (1<<APFEL_CHIPSELECT3) | (1<<APFEL_CHIPSELECT0) ); // only CS0 and CS3 are released, CS1 and CS2 are not influenced
- *-------------------------------------------------
- *
- * Manual byte per byte transmission and reading of the one byte reception buffer:
- *---------------------------------------------------------------------------------
- * uint8_t receivedByte;
- * apfelSetChosenChipSelect( 1 << APFEL_CHIPSELECT0 ); // set APFEL_CHIPSELECT0 (PORTB0) active
- * apfelWriteWithoutChipSelect( dataValue );  // clock out 8 bit dataValue
- * receivedByte = apfelReadByte();
- * apfelReleaseChoseChipSelect( 1 << APFEL_CHIPSELECT0 ); // release APFEL_CHIPSELECT0
- *
- */
 
 
 #ifndef APFEL_H_
@@ -80,6 +25,13 @@ enum apfelChipSelects
 	APFEL_PORT_ADDRESS_SET_5,
 	APFEL_PORT_ADDRESS_SET_MAXIMUM
 };
+
+typedef struct apfelAddressStruct
+{
+	char port;
+	uint8_t pinSetIndex;
+	uint8_t sideSelection;
+} apfelAddress;
 
 void apfelEnable(bool enable);
 
