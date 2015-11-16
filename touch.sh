b0d617bc5308c8145e90069983af7fb9c34842b4
zumbruch <p.zumbruch@gsi.de>
2015-11-16 14:13:33 +0100

    fixed a bug in touch (updating compile time for VERS command output)

--- touch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-fileList="api-version.c"
-for item in $(echo $fileList)
-do
-	[ -f $item ] && touch $item
-	echo touching $item
-	find obj_hadcon_2 -name ${item%%\.*}.o -delete
-	find obj_hadcon_2 -name ${item%%\.*}.lst -delete
-done
